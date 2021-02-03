e3484619ecfe5cbcbda520267425cb651b78d245
taiwo2 <adetonat@ymail.com>
2021-02-03 15:48:19 +0100

    Fix linter errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,34 +1,35 @@
 class UsersController < ApplicationController
-
-   def new
-      @user = User.new
-   end
-   
-   def create
-      #  @user =User.new(username: params[:username],email: params[:email], password params[:password])
-   
-     @user = User.new(params_user)
-     if @user.save
-     redirect_to new_user_path
-      else
+  def new
+    @user = User.new
+  end
+  
+  def create
+    #  @user =User.new(username: params[:username],email: params[:email], password params[:password])
+  
+    @user = User.new(params_user)
+    if @user.save
+      redirect_to new_user_path
+    else
       render :new
-      end
-   end
-   
-   def edit
-     @user = User.find(params[:id])
-   end  
-   def update
-      @user =User.find(params[:id])
-     if @user.update(params_user)
-       redirect_to new_user_path
-     else
-       render :edit
-     end
-   end
+    end
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end  
 
-   private
-   def params_user
-     params.require(:user).permit(:username, :email, :password, :password_confirmation)
-   end
+  def update
+    @user =User.find(params[:id])
+    if @user.update(params_user)
+      redirect_to new_user_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def params_user
+   params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
 end
