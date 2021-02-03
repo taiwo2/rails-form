3dd23f59fa98da3e2f1f6f4f44c4ad550ee12989
taiwo2 <adetonat@ymail.com>
2021-02-03 15:41:44 +0100

    Fix linter errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,48 +1,34 @@
 class UsersController < ApplicationController
 
-    # def show
-    #     @user = User.find(params[:id])
-    #   end
-
-    def new
-        @user = User.new
-    end
-    
-    def create
-       #  @user =User.new(username: params[:username],email: params[:email], password params[:password])
-    
-       @user = User.new(params_user)
-       if @user.save
-
-           redirect_to new_user_path
-       else
-           render :new
-       end
-    end
-    
-    def edit
-        @user = User.find(params[:id])
-    end
-    
-
-    def update
-      @user =User.find(params[:id])
-
-      if @user.update(params_user)
-         
-        redirect_to new_user_path
-        
-
+   def new
+      @user = User.new
+   end
+   
+   def create
+      #  @user =User.new(username: params[:username],email: params[:email], password params[:password])
+   
+     @user = User.new(params_user)
+     if @user.save
+     redirect_to new_user_path
       else
-        render :edit
+      render :new
       end
-    
-    end
+   end
+   
+   def edit
+     @user = User.find(params[:id])
+   end  
+   def update
+      @user =User.find(params[:id])
+     if @user.update(params_user)
+       redirect_to new_user_path
+     else
+       render :edit
+     end
+   end
 
-    private
-
-    def params_user
-        params.require(:user).permit(:username, :email, :password, :password_confirmation)
-    end
-
+   private
+   def params_user
+     params.require(:user).permit(:username, :email, :password, :password_confirmation)
+   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    validates :username, presence: true, uniqueness: true
-    validates :email, presence: true, uniqueness:true
-    validates :password, presence: true, confirmation: true
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, confirmation: true
 end
